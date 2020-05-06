1b6d8f9b5f286c7638ff9680c5156b5a7491540f
Junjie Chen <junjie.chen18@gmail.com>
2020-05-05 21:47:03 -0400

    feat(Boat): define pilot method

--- decorators.ts
+++ decorators.ts
@@ -4,4 +4,12 @@ class Boat {
   get getColor(): string {
     return `The color of the boat is ${this.color}`;
   }
+
+  pilot(engine: string, speed: string): void {
+    if (engine && speed) {
+      console.log('Boat is sailing');
+    } else {
+      throw new Error();
+    }
+  }
 }
